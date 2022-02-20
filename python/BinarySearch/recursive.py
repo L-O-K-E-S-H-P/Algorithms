744ad69e309a7b2868a489aaa9a6d514660f20dc
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2022-02-20 17:32:22 +0530

    Create recursive.py

---
+++ python/BinarySearch/recursive.py
@@ -0,0 +1,30 @@
+def binary_search(arr,low,high,x):
+  if (high>=low):
+    mid=(high+low)//2
+    # If element is present at the middle itself
+    if(arr[mid]==x):
+      return mid
+    # If element is smaller than mid, then it can only
+    # be present in left subarray
+    elif (arr[mid]>x):
+      return binary_search(arr,low,mid-1,x)
+    # Else the element can only be present in right subarray
+    else:
+      return binary_search(arr,mid+1,high,x)
+  else:
+    # Element is not present in the array
+    return -1
+arr=[25,14,32,1,6,8]
+x=32
+# call function
+result=binary_search(arr,0,len(arr)-1,x)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+'''
+A program is called recursive when an entity calls itself
+A program is call iterative when there is a loop (or repetition).
+'''
+      
