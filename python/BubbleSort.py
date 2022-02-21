f2e1c79e33e72b801264fdf543d4045c4d411f23
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2022-02-21 18:39:03 +0530

    Create BubbleSort.py

---
+++ python/BubbleSort.py
@@ -0,0 +1,19 @@
+#Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order. 
+
+
+def bubble_sort(arr):
+  for i in range(0,len(arr)-1):
+    for j in range(len(arr)-1):
+      if(arr[j]>arr[j+1]):
+        arr[j],arr[j+1]=arr[j+1],arr[j]
+  return arr
+arr=[5,3,8,6,7,2]
+print("The Unsorted Array is:",arr)
+print("The Sorted Array is:",bubble_sort(arr))
+
+
+''''
+Output:
+The Unsorted Array is: [5, 3, 8, 6, 7, 2]
+The Sorted Array is: [2, 3, 5, 6, 7, 8]
+'''
